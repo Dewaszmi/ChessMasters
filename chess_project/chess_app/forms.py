6d1af3fdcbcb6f32b98471e788e35e189b43efb6
michalozminkowski <michalozminkowski673@gmail.com>
2025-12-11 08:56:39 +0100

    Nowy, estetyczny panel logowania i rejestracji

--- chess_project/chess_app/forms.py
+++ chess_project/chess_app/forms.py
@@ -1,26 +1,48 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
-from .models import Profile
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label="Username", widget=forms.TextInput(attrs={"class": "form-control"}))
-    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    username = forms.CharField(
+        label="Nazwa użytkownika",
+        widget=forms.TextInput(attrs={
+            "class": "form-control form-control-lg",
+            "placeholder": "Twój login"
+        })
+    )
+    password = forms.CharField(
+        label="Hasło",
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control form-control-lg",
+            "placeholder": "Twoje hasło"
+        })
+    )
 
 class RegisterForm(UserCreationForm):
-
     email = forms.EmailField(
         label="Email",
         required=False,
-        widget=forms.EmailInput(attrs={"class": "form-control"})
+        widget=forms.EmailInput(attrs={
+            "class": "form-control",
+            "placeholder": "jan@example.com"
+        })
     )
 
     role = forms.ChoiceField(
         choices=Profile.ROLE_CHOICES,
-        label="Role",
-        widget=forms.Select(attrs={"class": "form-select"})
+        label="Rola",
+        widget=forms.Select(attrs={
+            "class": "form-select"
+        })
     )
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email")
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if 'class' not in self.fields[field].widget.attrs:
+                self.fields[field].widget.attrs['class'] = 'form-control'
